7d687daf2c8bf0ba5990e34f5ce0c602130e1068
Matt Florence <mflorence@onetechnologies.net>
2013-03-25 18:13:18 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flexevent::Application.config.secret_token = 'a34e96f921c4658e53150dbb55c61bd0c4035f163f520b3e3eaff47dba145696f3e0ca5aef929b0880e42bcbc517bec84bbc4a9319dcf3506be5cfbdc469fc55'
