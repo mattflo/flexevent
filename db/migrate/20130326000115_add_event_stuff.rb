2c14a8fd4ceef186d63e35626806dbd1dc206774
Matt Florence <mflorence@onetechnologies.net>
2013-03-25 19:11:57 -0500

    migrate new event fields

--- db/migrate/20130326000115_add_event_stuff.rb
+++ db/migrate/20130326000115_add_event_stuff.rb
@@ -1,6 +1,6 @@
 class AddEventStuff < ActiveRecord::Migration
   def up
-  	add_column :events, :event_time, :timestamp
+  	add_column :events, :event_time, :time
   	add_column :events, :voting_cutoff, :timestamp
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(:version => 20130326000115) do
     t.string   "address"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
-    t.datetime "event_time"
+    t.time     "event_time"
     t.datetime "voting_cutoff"
   end
 
