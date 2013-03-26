bf94d6821b1c742976fdbb5d0ba9c2ba334f2ee6
Matt Florence <mflorence@onetechnologies.net>
2013-03-25 22:52:30 -0500

    scrubbing email

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -2,4 +2,12 @@ class Voter < ActiveRecord::Base
   attr_accessible :email, :vote_id, :event_id
   belongs_to :vote
   belongs_to :event
+  before_save :default_values
+  def default_values
+  	self.guid = SecureRandom.uuid
+  end
+  def email_scrubbed
+  	return "" unless self.email
+  	self.email[0,2] + '***@***' + self.email[-6,6] 
+  end
 end
