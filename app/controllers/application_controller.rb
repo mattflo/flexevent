3e0d3d6d3f517a842b1d5f3999f7e67b350b976d
Matt Florence <mflorence@onetechnologies.net>
2013-03-26 08:48:17 -0500

    cheap security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :handle_cookies
+  before_filter :secure, :only => [:new, :create, :edit, :destroy]
+  def handle_cookies
+    cookies.permanent[:_flexevent_admin_key] = "set me" if cookies[:_flexevent_admin_key].nil?
+  end
+  def secure
+  	# puts cookies[:_flexevent_admin_key] 
+  	render :nothing => true, :status => 403 unless cookies[:_flexevent_admin_key] == ENV['FLEX_EVENT_ADMIN_KEY']
+  end
 end
